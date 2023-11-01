cfd63b2fd50e477233e4707581245223b98611f6
Lord Machiavelli <tmakaveli643@gmail.com>
2023-11-01 23:11:15 +0100

    ex1

---
+++ JAVA/frst/TP1/EX1/Car.java
@@ -0,0 +1,32 @@
+package TP1.EX1;
+
+public class Car {
+    public String brand; 
+    public double price; 
+    Car() {
+        this.brand = "default";
+        this.price = 0.0;
+    }
+    //getters and setters 
+   public String getBrand() {
+    return this.brand;
+   }
+   public double getPrice (){
+    return this.price; 
+   }
+
+
+   public void setPrice (double price) {
+    this.price = price; 
+
+   }
+
+   public void setBrand(String brand)  {
+    this.brand = brand ; 
+   }
+
+   public void Show() {
+    System.out.println("Brand: " + this.brand + " Price: " + this.price);
+   }
+
+}
---
+++ JAVA/frst/TP1/EX1/Main.java
@@ -0,0 +1,12 @@
+package TP1.EX1;
+
+public class Main  {
+    public static void main(String args[]) {
+        
+        Car bmw = new Car(); 
+        bmw.setBrand("bmw");
+        bmw.setPrice(1000000.0);
+        bmw.Show();
+        
+    }
+}
